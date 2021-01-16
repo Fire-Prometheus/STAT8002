11a451443eb84f37f043f743fc734e2590177f6c
Fire-Prometheus <alextowunyin@gmail.com>
2021-01-16 12:17:33 +0800

    Uncomment the code.

--- python/preprocessing/preprocessing.py
+++ python/preprocessing/preprocessing.py
@@ -1,22 +1,22 @@
 from python.preprocessing import NewsDataPreprocessor, PriceDataPreprocessor, PICKLE
-#
-# GRAINS = ['corn', 'soybean', 'wheat', 'rice', 'oat']
-#
-# for g in GRAINS:
-#     news_data_preprocessor = NewsDataPreprocessor('../data/agricultural news from 2017.csv', g)
-#     news_data_preprocessor.save_as_pickle(PICKLE['NEWS'][g.upper()])
-#
-# price_data_preprocessor = PriceDataPreprocessor('../data/US Corn Futures Historical Data.csv')
-# price_data_preprocessor.save_as_pickle(PICKLE['PRICE']['CORN'])
-#
-# price_data_preprocessor = PriceDataPreprocessor('../data/US Soybeans Futures Historical Data.csv')
-# price_data_preprocessor.save_as_pickle(PICKLE['PRICE']['SOYBEAN'])
-#
+
+GRAINS = ['corn', 'soybean', 'wheat', 'rice', 'oat']
+
+for g in GRAINS:
+    news_data_preprocessor = NewsDataPreprocessor('../data/agricultural news from 2017.csv', g)
+    news_data_preprocessor.save_as_pickle(PICKLE['NEWS'][g.upper()])
+
+price_data_preprocessor = PriceDataPreprocessor('../data/US Corn Futures Historical Data.csv')
+price_data_preprocessor.save_as_pickle(PICKLE['PRICE']['CORN'])
+
+price_data_preprocessor = PriceDataPreprocessor('../data/US Soybeans Futures Historical Data.csv')
+price_data_preprocessor.save_as_pickle(PICKLE['PRICE']['SOYBEAN'])
+
 price_data_preprocessor = PriceDataPreprocessor('../data/US Wheat Futures Historical Data.csv')
 price_data_preprocessor.save_as_pickle(PICKLE['PRICE']['WHEAT'])
 
-# price_data_preprocessor = PriceDataPreprocessor('../data/Rough Rice Futures Historical Data.csv')
-# price_data_preprocessor.save_as_pickle(PICKLE['PRICE']['RICE'])
+price_data_preprocessor = PriceDataPreprocessor('../data/Rough Rice Futures Historical Data.csv')
+price_data_preprocessor.save_as_pickle(PICKLE['PRICE']['RICE'])
 
-# price_data_preprocessor = PriceDataPreprocessor('../data/Oats Futures Historical Data.csv')
-# price_data_preprocessor.save_as_pickle(PICKLE['PRICE']['OAT'])
+price_data_preprocessor = PriceDataPreprocessor('../data/Oats Futures Historical Data.csv')
+price_data_preprocessor.save_as_pickle(PICKLE['PRICE']['OAT'])
